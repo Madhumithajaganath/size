372a9650e1c4b29841be9ec053af4d0f944a2dab
Madhumithajaganath <90049051+Madhumithajaganath@users.noreply.github.com>
2021-10-02 17:04:01 +0530

    Update and rename README.md to Size.cpp

---
+++ Size.cpp
@@ -0,0 +1,8 @@
+#include<iostream.h>
+void main()
+{
+    cout<<"size of int :"<<sizeof(int)<<" bytes \n";
+    cout<<"size of float:"<<sizeof(float)<<" bytes \n";
+    cout<<"size of char:"<<sizeof(char)<<" bytes \n";
+    cout<<"size of double:"<<sizeof(double)<<" bytes \n";
+}
